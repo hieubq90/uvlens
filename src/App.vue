5ec3c030886998454eb36a1c80f28ab40c6b3a0b
hieubq <hieubq90@gmail.com>
2019-05-19 14:04:59 +0700

    update layout with fullpage-vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,65 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="fullpage-container">
+    <div class="button-group">
+      <button type="button" :class="{active:index ==0}" @click="moveTo(0)">
+        {{ curentTime | moment("ngày DD-MM-YYYY") }}
+      </button>
+      <button type="button" :class="{active:index ==1}" @click="moveTo(1)">
+        {{ curentTime | moment("add", "1 day", "ngày DD-MM-YYYY") }}
+      </button>
     </div>
-    <router-view/>
+    <div class="fullpage-wp" v-fullpage="opts" ref="fullpage">
+      <div class="page-1 page">
+        <p class="part-1" v-animate="{value: 'bounceInLeft'}">
+          {{ curentTime | moment("Biểu đồ giá trị UV ngày DD-MM-YYYY") }}
+        </p>
+      </div>
+      <div class="page-2 page">
+        <p class="part-2" v-animate="{value: 'bounceInRight'}">
+          {{ curentTime | moment("add", "1 day", "Biểu đồ giá trị UV ngày DD-MM-YYYY") }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'app',
+  data() {
+    const that = this;
+    return {
+      index: 0,
+      curentTime: new Date(),
+      opts: {
+        start: 0,
+        dir: 'v',
+        duration: 500,
+        beforeChange: (currentSlideEl, currenIndex, nextIndex) => {
+          console.log('beforeChange', currentSlideEl, currenIndex, nextIndex);
+          that.index = nextIndex;
+        },
+        afterChange: (currentSlideEl, currenIndex) => {
+          that.index = currenIndex;
+          console.log('afterChange', currentSlideEl, currenIndex);
+        },
+      },
+    };
+  },
+  methods: {
+    moveTo(index) {
+      this.$refs.fullpage.$fullpage.moveTo(index, true);
+    },
+  },
+};
+</script>
+
+
 <style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,14 +67,51 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.fullpage-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  .page {
+    display: block;
+    text-align: center;
+    font-size: 26px;
+    color: #eee;
   }
+  .page-1 {
+    padding-top: 100px;
+    background: #40B883;
+  }
+  .page-2 {
+    padding-top: 100px;
+    background: #0798EC;
+  }
+  .page-3 {
+    padding-top: 50px;
+    background: #aabbcc;
+  }
+  .button-group {
+        position: absolute;
+        top: 30px;
+        left: 30px;
+        z-index: 9;
+    }
+    .button-group button {
+        display: inline-block;
+        margin: 10px;
+        color: #000;
+        background: #fff;
+        background: rgba(255, 255, 255, .5);
+        -webkit-border-radius: 10px;
+        border-radius: 10px;
+        padding: 9px 18px;
+        border: none;
+        outline: none;
+    }
+    .button-group button.active {
+        background: rgba(0, 0, 0, .5);
+        color: #fff;
+    }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import Vue from 'vue';
+import 'animate.css';
+import 'fullpage-vue/src/fullpage.css';
+import VueFullpage from 'fullpage-vue';
+import VueMoment from 'vue-moment';
 import App from './App.vue';
-import router from './router';
+// import router from './router';
 import store from './store';
+
+
+Vue.use(VueFullpage);
+Vue.use(VueMoment);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
+  // router,
   store,
   render: h => h(App),
 }).$mount('#app');
