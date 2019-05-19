c9d3c2a50c79b0f1ebae13566659abaf69523c74
hieubq <hieubq90@gmail.com>
2019-05-20 02:58:57 +0700

    add chart

--- src/App.vue
+++ src/App.vue
@@ -13,24 +13,92 @@
         <h2 class="part-1" v-animate="{value: 'bounceInLeft'}">
           {{ curentTime | moment("BIỂU ĐỒ UV | DD-MM-YYYY") }}
         </h2>
-        <div v-if="loadedCurrentUV && loadedUVForcat">
-          <p class="part-1" v-animate="{value: 'bounceInLeft'}">
+        <div v-if="loadedCurrentUV && loadedUVForcast">
+          <img alt="weather_icon" :src="getWeatherIcon(currentWeatherIcon)">
+          <p class="part-1" v-animate="{value: 'bounceInRight'}">
             <span>{{ curentTime | moment("hh giờ mm") }} phút </span>
-            <img alt="Vue logo" :src="getWeatherIcon(currentWeatherIcon)">
-            <span> - UV: {{ currentUV }} - </span>
-            <span>{{ currentTemperature }}°C</span>
+            <span> - UV: {{ currentUV }} </span>
+            <span> - Nhiệt độ: {{ currentTemperature }}°C</span>
           </p>
+          <div class="chart-wrapper">
+            <la-cartesian
+              :data="getUVForcast(0)"
+              :bound="[0, 15]"
+              autoresize
+            >
+              <defs>
+                <linearGradient id="color-id" x1="0" y1="1" x2="0" y2="0">
+                  <stop offset="0" stop-color="#7DB637"></stop>
+                  <stop offset="0.2" stop-color="#F9B308"></stop>
+                  <stop offset="0.6" stop-color="#EF8617"></stop>
+                  <stop offset="0.8" stop-color="#E04127"></stop>
+                  <stop offset="1" stop-color="#A75E99"></stop>
+                </linearGradient>
+              </defs>
+              <!-- <la-area fill-color="url(#color-id)" dot curve prop="uv"></la-area> -->
+              <la-line curve :width="15" label="uv" prop="uv" color="url(#color-id)"></la-line>
+              <la-x-axis prop="name" color="white"></la-x-axis>
+              <la-y-axis color="white"></la-y-axis>
+              <la-tooltip>
+                <div class="tooltip" slot-scope="props">
+                  <div class="title">{{ props.label }}</div>
+                  <ul class="list">
+                    <li
+                      :key="item.label"
+                      v-for="item in props.actived"
+                      :style="{ borderTop: '3px solid white' }">
+                      <div class="value">UV: {{ item.value }}</div>
+                    </li>
+                  </ul>
+                </div>
+              </la-tooltip>
+            </la-cartesian>
+          </div>
         </div>
         <div v-else>
-          <p class="part-1" v-animate="{value: 'bounceInLeft'}">Đang tải dữ liệu ...</p>
+          <p class="part-1" v-animate="{value: 'bounceInRight'}">Đang tải dữ liệu ...</p>
         </div>
       </div>
       <div class="page-2 page">
         <p class="part-2" v-animate="{value: 'bounceInRight'}">
           {{ curentTime | moment("add", "1 day", "BIỂU ĐỒ UV | DD-MM-YYYY") }}
         </p>
-        <div v-if="loadedCurrentUV && loadedUVForcat">
+        <div v-if="loadedCurrentUV && loadedUVForcast">
           <p class="part-1" v-animate="{value: 'bounceInLeft'}">Đã tải xong dữ liệu</p>
+          <div class="chart-wrapper">
+            <la-cartesian
+              :data="getUVForcast(1)"
+              :bound="[0, 15]"
+              autoresize
+            >
+              <defs>
+                <linearGradient id="color-id" x1="0" y1="1" x2="0" y2="0">
+                  <stop offset="0" stop-color="#7DB637"></stop>
+                  <stop offset="0.2" stop-color="#F9B308"></stop>
+                  <stop offset="0.6" stop-color="#EF8617"></stop>
+                  <stop offset="0.8" stop-color="#E04127"></stop>
+                  <stop offset="1" stop-color="#A75E99"></stop>
+                </linearGradient>
+              </defs>
+              <!-- <la-area fill-color="url(#color-id)" dot curve prop="uv"></la-area> -->
+              <la-line curve :width="15" label="uv" prop="uv" color="url(#color-id)"></la-line>
+              <la-x-axis prop="name" color="white"></la-x-axis>
+              <la-y-axis color="white"></la-y-axis>
+              <la-tooltip>
+                <div class="tooltip" slot-scope="props">
+                  <div class="title">{{ props.label }}</div>
+                  <ul class="list">
+                    <li
+                      :key="item.label"
+                      v-for="item in props.actived"
+                      :style="{ borderTop: '3px solid white' }">
+                      <div class="value">UV: {{ item.value }}</div>
+                    </li>
+                  </ul>
+                </div>
+              </la-tooltip>
+            </la-cartesian>
+          </div>
         </div>
         <div v-else>
           <p class="part-1" v-animate="{value: 'bounceInLeft'}">Đang tải dữ liệu ...</p>
@@ -52,12 +120,12 @@ export default {
     return {
       index: 0,
       loadedCurrentUV: false,
-      loadedUVForcat: false,
+      loadedUVForcast: false,
       curentTime: new Date(),
       currentUV: 0,
       currentTemperature: 0,
       currentWeatherIcon: 0,
-      uvForcat: [],
+      uvForcast: [],
       opts: {
         start: 0,
         dir: 'v',
@@ -78,7 +146,7 @@ export default {
       this.$refs.fullpage.$fullpage.moveTo(index, true);
     },
     getWeatherIcon(value) {
-      return `./assets/weather_${value}.png`;
+      return `https://uvlens.hieubq.name.vn/assets/weather_${value}.png`;
     },
     getUVDatas() {
       // get current uv
@@ -91,9 +159,21 @@ export default {
       });
       apiService.getDailyUV().then((data) => {
         console.log('dailyUV', data);
-        this.uvForcat = data.DailyForecasts;
-        this.loadedUVForcat = true;
+        this.uvForcast = data.DailyForecasts;
+        this.loadedUVForcast = true;
       });
+    },
+    getUVForcast(index) {
+      if (this.uvForcast.length > 0 && (index <= 1 || index >= 0)) {
+        const data = this.uvForcast[index].UVForecast.filter((value, i) => i < 13)
+          .map((value, i) => ({
+            name: `${i + 6} giờ`,
+            uv: value,
+          }));
+        console.log('data', data);
+        return data;
+      }
+      return [];
     },
     geolocation() {
       navigator.geolocation.getCurrentPosition(this.geoSuccess, this.geoError);
@@ -127,6 +207,41 @@ body {
   text-align: center;
   color: #2c3e50;
 }
+.tooltip {
+  background: rgba(0, 0, 0, 0.8);
+  border-radius: 4px;
+}
+
+.title {
+  padding: 10px;
+  color: #fff;
+}
+
+.list {
+  list-style: none;
+  display: flex;
+  padding: 0;
+}
+
+.list li {
+  flex: 1;
+  color: #fff;
+  padding: 5px;
+  min-width: 100px;
+}
+
+.list li::before {
+  content: none;
+}
+
+.label {
+  color: #fff;
+  font-weight: 600;
+}
+
+.value {
+  color: #fff;
+}
 .fullpage-container {
   position: absolute;
   left: 0;
@@ -138,6 +253,9 @@ body {
     text-align: center;
     font-size: 16px;
     color: #eee;
+    img {
+      width: 100px;
+    }
   }
   .page-1 {
     padding-top: 100px;
@@ -157,21 +275,21 @@ body {
         left: 30px;
         z-index: 9;
     }
-    .button-group button {
-        display: inline-block;
-        margin: 10px;
-        color: #000;
-        background: #fff;
-        background: rgba(255, 255, 255, .5);
-        -webkit-border-radius: 10px;
-        border-radius: 10px;
-        padding: 9px 18px;
-        border: none;
-        outline: none;
-    }
-    .button-group button.active {
-        background: rgba(0, 0, 0, .5);
-        color: #fff;
-    }
+  .button-group button {
+      display: inline-block;
+      margin: 10px;
+      color: #000;
+      background: #fff;
+      background: rgba(255, 255, 255, .5);
+      -webkit-border-radius: 10px;
+      border-radius: 10px;
+      padding: 9px 18px;
+      border: none;
+      outline: none;
+  }
+  .button-group button.active {
+      background: rgba(0, 0, 0, .5);
+      color: #fff;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import 'animate.css';
 import 'fullpage-vue/src/fullpage.css';
 import VueFullpage from 'fullpage-vue';
 import VueMoment from 'vue-moment';
+import { Laue } from 'laue';
+
 import App from './App.vue';
 // import router from './router';
 import store from './store';
@@ -10,6 +12,7 @@ import store from './store';
 
 Vue.use(VueFullpage);
 Vue.use(VueMoment);
+Vue.use(Laue);
 
 Vue.config.productionTip = false;
 
